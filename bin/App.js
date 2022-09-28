32691935397a4e211d391e0727f317d36ff82a23
PanGooGoo <antoniocimmino8@gmail.com>
2022-09-28 19:06:00 +0100

    First Deploy

---
+++ bin/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+function App() {
+  return (
+    <div >
+      <img src="https://gateway.pinata.cloud/ipfs/Qmb75SBbD27DzQkmsQzybKJBtnaGqT6z946xsS5EjKA87p"
+        height="100%" width="100%" alt="image" />
+    </div>
+  );
+}
+
+export default App;
